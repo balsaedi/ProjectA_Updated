5299d25499e6b440caf50376848d360b7705c04e
Basim Alsaedi <33208704+balsaedi@users.noreply.github.com>
2024-01-16 15:57:27 +0300

    commit2

---
+++ renderBook.R
@@ -0,0 +1,4 @@
+bookdown::render_book("chapters/index.Rmd",
+                      output_dir = "_book",
+                      output_format = "bookdown::gitbook")
+#devtools::install_github("datacamp/tutorial")
